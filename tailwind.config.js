92e2ccc89c42e5228e29667aa060401e4dd994eb
Harry Gulliford <harry@harrygulliford.com>
2019-09-12 12:12:12 +1000

    Add site

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: '#492F92',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    screens: {
+      // 'sm': '640px',
+      // 'md': '768px',
+      'lg': '1024px',
+      // 'xl': '1280px',
+    }
+  },
+  variants: {},
+  plugins: [],
+}
